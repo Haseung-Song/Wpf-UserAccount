101fa2c75271ac238919e067450a8331d8998e63
Haseung-Song <tkrmtkrm1245@naver.com>
2024-06-15 23:39:39 +0900

    초기 커밋.

---
+++ Wpf-UserAccount/Converters/SecureStringHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Wpf_UserAccount.Converters
+{
+    public static class SecureStringHelper
+    {
+        public static SecureString ConvertToSecureString(string password)
+        {
+            if (password == null)
+            {
+                return null;
+            }
+            SecureString securePassword = new SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+            securePassword.MakeReadOnly();
+            return securePassword;
+        }
+
+        public static string ConvertToUnsecureString(SecureString securePassword)
+        {
+            if (securePassword == null)
+            {
+                return string.Empty;
+            }
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message: {ex.Message}");
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+            return Marshal.PtrToStringUni(unmanagedString);
+        }
+
+    }
+
+}
---
+++ Wpf-UserAccount/Views/LoginView.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Wpf_UserAccount.ViewModels;
+
+namespace Wpf_UserAccount.Views
+{
+    /// <summary>
+    /// LoginView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        private bool isDoubleClicking = true;
+        private readonly double originalWidth, originalHeight = 0;
+        readonly ScaleTransform scale = new ScaleTransform();
+
+        /// <summary>
+        /// [로그인 화면]
+        /// </summary>
+        public LoginView()
+        {
+            InitializeComponent();
+            DataContext = new LoginVM();
+            originalWidth = Width;
+            originalHeight = Height;
+            SizeChanged += Window_SizeChanged;
+        }
+
+        /// <summary>
+        /// [Window 크기 변환] 이벤트 발생
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ChangeSize(e.NewSize.Width, e.NewSize.Height); // New 너비 및 높이
+        }
+
+        /// <summary>
+        /// [New 너비 및 높이] 메서드 함수
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        private void ChangeSize(double width, double height)
+        {
+            scale.ScaleX = width / originalWidth;
+            scale.ScaleY = height / originalHeight;
+            FrameworkElement rootElement = Content as FrameworkElement;
+            rootElement.LayoutTransform = scale;
+        }
+
+        /// <summary>
+        /// [Mouse 좌클릭] 이벤트 발생
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+        }
+
+        /// <summary>
+        /// [최소화 버튼] 이벤트 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// [최대화 버튼] 이벤트 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (isDoubleClicking)
+            {
+                WindowState = WindowState.Maximized;
+                ChangeSize(ActualWidth, ActualHeight);
+                isDoubleClicking = false;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+                isDoubleClicking = true;
+            }
+
+        }
+
+        /// <summary>
+        /// [창닫기 버튼] 이벤트 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+
+}
